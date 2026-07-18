e0d08e75227c5ce5aa3458cdad008c1bd3f5339c
GitHub <fantasy-q@users.noreply.github.com>
2026-07-19 04:45:17 +0800

    fix: path error

--- docs/ooo/bombs/main.js
+++ docs/ooo/bombs/main.js
@@ -12,7 +12,7 @@ const bombs = [
   'Emoji',
   'Hako',
 ];
-const BASE_URL = '/game/assets/Ooo/sprites/sBomb/';
+const BASE_URL = '/game/assets/ooo/sprites/sBomb/';
 
 // App
 const App = document.querySelector('#app');
